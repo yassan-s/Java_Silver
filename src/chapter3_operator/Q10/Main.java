a8fff091b1d260f800b1530b4ecb80f45b507b01
yassan-s <yasuro.2103@gmail.com>
2022-01-26 22:58:11 +0900

    3章 equalsメソッドを検証

--- src/chapter3_operator/Q10/Main.java
+++ src/chapter3_operator/Q10/Main.java
@@ -4,12 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Sample a = new Sample(10);
-		Sample b = new Sample(10);
+		// Object
+		Object a = new Sample(10);
+		Object b = new Sample(10);
+		// Sample
+		Sample c = new Sample(10);
+		Sample d = new Sample(10);
+		// System.out.println(b.num);
 
+		System.out.println(a == b); //false
 		System.out.println(a.equals(b)); //false
-		System.out.println(a.getThis()); //chapter3_operator.Q10.Sample@7344699f
-		System.out.println(b.getThis()); //chapter3_operator.Q10.Sample@6b95977
+		System.out.println(c == d); //false
+		System.out.println(c.equals(d)); //true
+		System.out.println(c.getThis()); //chapter3_operator.Q10.Sample@7344699f
+		System.out.println(d.getThis()); //chapter3_operator.Q10.Sample@6b95977
 
 	}
 
--- src/chapter3_operator/Q10/Sample.java
+++ src/chapter3_operator/Q10/Sample.java
@@ -1,10 +1,17 @@
 package chapter3_operator.Q10;
 
 public class Sample {
-	private int num;
+	 private int num;
 
 	public Sample(int num) {
 		this.num = num;
+	}
+
+	public boolean equals(Sample obj) {
+		if (obj == null) {
+			return false;
+		}
+		return this.num == obj.num;
 	}
 
 	public Sample getThis() {
