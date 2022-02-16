0104fcbf13687b98cf7968b05a08aad902c0b649
yassan-s <yasuro.2103@gmail.com>
2022-02-16 23:58:37 +0900

    9章 Mathクラスを検証

---
+++ src/chapter9_API/Q01/Main.java
@@ -0,0 +1,16 @@
+package chapter9_API.Q01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 毎年積立を行って複利運用した時の金額
+		int n;
+		int m;
+		int r;
+		int x;
+
+		n = (int)(m * (Math.pow(1 + r), x) - 1) / r);
+
+	}
+
+}
